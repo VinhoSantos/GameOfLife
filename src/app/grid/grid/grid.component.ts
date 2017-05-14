14a1d9f2ed4c12df97f7a8eff646234155c7108a
Bart Vets <bart.vets@qframe.be>
2017-05-14 02:47:25 +0200

    creating grid and setting start state

--- src/app/grid/grid/grid.component.ts
+++ src/app/grid/grid/grid.component.ts
@@ -10,23 +10,42 @@ export class GridComponent implements OnInit {
   grid: boolean[][] = [];
   gridSize: number = 50;
 
+  started: boolean = false;
+  ended: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
     this.createEmptyGrid();
+    this.setStartState();
   }
 
-  createEmptyGrid = () => {
+  private createEmptyGrid = () => {
     this.grid = [];
     for (let x = 0; x < this.gridSize; x++) { //rows
       this.grid[x] = [];
       for (let y = 0; y < this.gridSize; y++) { //columns
-        //this.grid[x][y] = false;
-        if (x == 20 && y == 25) {
-          //this.grid[x][y] = true;
-        }
+        this.grid[x][y] = false;
       }
     }
+  }
+
+  private setStartState = () => {
+    this.grid[4][5] = true;
+    this.grid[5][6] = true;
+    this.grid[6][4] = true;
+    this.grid[6][5] = true;
+    this.grid[6][6] = true;
+  }
+
+  run = () => {
+    this.started = true;
+    this.ended = false;
+  }
+
+  end = () => {
+    this.ended = true;
+    this.started = false;
   }
 
 }
